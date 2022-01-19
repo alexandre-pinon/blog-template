34d329f5cdd9f0cd2180d7161ca38089a393af70
Alexandre Pinon <alexandre.pinon@epitech.eu>
2022-01-19 09:50:32 +0100

    Recent Posts Widget

--- services/index.ts
+++ services/index.ts
@@ -37,3 +37,44 @@ export const getPosts = async () => {
 
   return result.postsConnection.edges
 }
+
+export const getRecentPosts = async () => {
+  const query = gql`
+    query GetPostDetails() {
+      posts(
+        orderBy: createdAt_ASC
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `
+
+  const result = await request(GRAPHQL_API, query)
+
+  return result.posts
+}
+
+export const getSimilarPosts = async (categories, slug) => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]) {
+      posts(where: { slug_not: $slug, AND: { categories_some: { slug_in: $categories } } }, last: 3) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `
+
+  const result = await request(GRAPHQL_API, query)
+
+  return result.posts
+}
